8b23abbc83094253589fda092a2c9dd54d2ac9af
Iain Breen <iain.breen@gmail.com>
2017-05-31 00:52:27 +0100

    more appspec.yml tests...

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/weatherassignment
 # RAILS_ENV=development bundle install --path vendor/bundle
-RAILS_ENV=development bundle exec rake db:migrate
+#RAILS_ENV=development bundle exec rake db:migrate
 # RAILS_ENV=development bundle exec rake assets:clobber
 # RAILS_ENV=development bundle exec rake assets:precompile
