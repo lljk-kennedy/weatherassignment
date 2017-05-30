1f966e12acc40fedc94849449d82a2a3aff82fd8
Iain Breen <iain.breen@gmail.com>
2017-05-30 23:23:43 +0100

    deployment changes, setting permissions in appspec and circle, setting console logging in application

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Weather2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.web_console.whitelisted_ips = '172.31.0.0/16'
   end
 end
