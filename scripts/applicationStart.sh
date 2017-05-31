dc435f5898b0eec5a0c6a639444fb4d9f308427a
Iain Breen <iain.breen@gmail.com>
2017-05-31 11:17:09 +0100

    changing rails server exe start

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -2,4 +2,4 @@
 cd /var/www/
 sudo chown -R ubuntu:ubuntu weatherassignment/
 cd /var/www/weatherassignment/
-/home/ubuntu/.rvm/gems/ruby-2.4.0/wrappers/rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
+rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
