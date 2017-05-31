8529bd8fb407e5620e68c223cd7d5e5e5894e457
Iain Breen <iain.breen@gmail.com>
2017-05-31 12:02:05 +0100

    adding back in afterinstall in appspec.yml

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd /var/www/weatherassignment
-RAILS_ENV=development bundle install --path vendor/bundle
+cd /var/www/weatherassignment && bundle install --path vendor/bundle
 #RAILS_ENV=development bundle exec rake db:migrate
 # RAILS_ENV=development bundle exec rake assets:clobber
 # RAILS_ENV=development bundle exec rake assets:precompile
