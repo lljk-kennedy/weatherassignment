94c43f68aa8b7385255a66d248c4dd43bc77b7ee
Iain Breen <iain.breen@gmail.com>
2017-05-31 01:39:40 +0100

    more appspec.yml tests...

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /var/www/
 sudo chown -R ec2-user:ec2-user weatherassignment/
-cd /var/www/weatherassignment/
- /home/ec2-user/.rvm/gems/ruby-2.4.0/wrappers/rails server 
+# cd /var/www/weatherassignment/
+#  /home/ec2-user/.rvm/gems/ruby-2.4.0/wrappers/rails server 
