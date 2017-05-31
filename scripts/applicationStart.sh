19ca10c8dbad3aafa394ac53aab8032675957c59
Iain Breen <iain.breen@gmail.com>
2017-05-31 11:11:05 +0100

    moved from AL to Ubuntu, changing run-as user

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /var/www/
-sudo chown -R ec2-user:ec2-user weatherassignment/
+sudo chown -R ubuntu:ubuntu weatherassignment/
 cd /var/www/weatherassignment/
-/home/ec2-user/.rvm/gems/ruby-2.4.0/wrappers/rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
+/home/ubuntu/.rvm/gems/ruby-2.4.0/wrappers/rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
