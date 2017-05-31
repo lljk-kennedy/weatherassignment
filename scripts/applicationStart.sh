b1f50e9f76b59633bec102dfb4e5564297940e6b
Iain Breen <iain.breen@gmail.com>
2017-05-31 11:43:05 +0100

    adding bundle install command to afterinstall script

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -2,5 +2,5 @@
 cd /var/www/
 sudo chown -R ubuntu:ubuntu weatherassignment/
 cd /var/www/weatherassignment/
-bundle install 
+#bundle install 
 /home/ubuntu/.rbenv/shims/rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
