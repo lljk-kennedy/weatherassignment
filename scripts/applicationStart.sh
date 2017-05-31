f91f5ab826a2a6ebab6b6700288d8cdedca2f5f3
Iain Breen <iain.breen@gmail.com>
2017-05-31 11:21:42 +0100

    correcting rails start location

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -2,4 +2,4 @@
 cd /var/www/
 sudo chown -R ubuntu:ubuntu weatherassignment/
 cd /var/www/weatherassignment/
-rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
+/home/ubuntu/.rbenv/shims/rails server -d -p 3000 -b 0.0.0.0 --dev-caching 
