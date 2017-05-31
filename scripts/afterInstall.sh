2d1de6eb2bad425ff6d4b3d02279cdf4597173ac
Iain Breen <iain.breen@gmail.com>
2017-05-31 12:15:21 +0100

    setting app permissions

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /var/www/
+sudo chown -R ubuntu:ubuntu weatherassignment/
 cd /var/www/weatherassignment 
 /home/ubuntu/.rbenv/shims/bundle install --path vendor/bundle
 #RAILS_ENV=development bundle exec rake db:migrate
