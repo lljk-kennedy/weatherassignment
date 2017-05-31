c12b8cfe47d16b9a6352420b3f0f948d62ceda2f
Iain Breen <iain.breen@gmail.com>
2017-05-31 12:06:13 +0100

    changing bundle path

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /var/www/weatherassignment && bundle install --path vendor/bundle
+cd /var/www/weatherassignment 
+/home/ubuntu/.rbenv/shims/bundle install --path vendor/bundle
 #RAILS_ENV=development bundle exec rake db:migrate
 # RAILS_ENV=development bundle exec rake assets:clobber
 # RAILS_ENV=development bundle exec rake assets:precompile
