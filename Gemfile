a27f282eafd90aed61d5512c73ba0c32729cf783
Iain Breen <iain.breen@gmail.com>
2017-05-31 11:39:09 +0100

    adding bundle gem

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem 'bundler'
 gem 'rest-client'
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'jquery-rails'
--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /var/www/weatherassignment
-bundle install 
-bin/rails db:migrate RAILS_ENV=development
+# cd /var/www/weatherassignment
+# bundle install 
+#RAILS_ENV=development bundle exec rake db:migrate
 # RAILS_ENV=development bundle exec rake assets:clobber
 # RAILS_ENV=development bundle exec rake assets:precompile
